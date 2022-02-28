a6b801be73a1ece5434b2ed1f3c6c31dfe019e61
Keranak <kerrianlebras@hotmail.com>
2022-02-28 12:59:15 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*Déclaration des mes variables*/
+
+let btnServer = document.getElementById('btnServer'),
+  btnUhc = document.getElementById('btnUhc');
+
+
+/*Script*/
+
